2f737a01e5b1b8e84d6b23a86d841e26262f65af
camyllasantos <camyllastudys@gmail.com>
2023-12-18 09:02:48 -0300

    novas mmudanças

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const form =document.getElementById('form-list')
+const form = document.getElementById('form-list')
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const inputAdicioneNome = document.getElementById('adicione-nome');
+    const inputAdicioneNumero = document.getElementById('adicione-numero');
+
+    alert(`Adicione: ${inputAdicioneNome.value} - Numero: ${inputAdicioneNumero.value} `);
+});
+
+
