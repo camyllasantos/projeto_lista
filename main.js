d67071952507bfa454152de182f7ef7140b7f65e
camyllasantos <camyllastudys@gmail.com>
2023-12-19 12:53:41 -0300

    fix: correção de linhas

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const form = document.getElementById('form-list')
+const form = document.getElementById('form-list');
+let linhas = '';
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     
     const inputAdicioneNome = document.getElementById('adicione-nome');
     const inputAdicioneNumero = document.getElementById('adicione-numero');
 
-    alert(`Adicione: ${inputAdicioneNome.value} - Numero: ${inputAdicioneNumero.value} `);
-});
+    let linha = '<tr>';
+    linha += `<td>${inputAdicioneNome.value}</td>`;
+    linha += `<td>${inputAdicioneNumero.value}</td>`;
+    linha += '</tr>';
 
+    linhas += linha;
 
+    const linhaTabela = document.querySelector('tbody');
+    linhaTabela.innerHTML = linhas;
+
+    inputAdicioneNome.value = '';
+    inputAdicioneNumero.value = '';
+})
